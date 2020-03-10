6c6208d6824a200b1e50275d2285699065f85737
hamedsalim <hamediran1999@gmail.com>
2020-03-10 15:46:58 +0330

    add temp

--- school/tutorial/views.py
+++ school/tutorial/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render , redirect
+from django.views.generic import TemplateView,ListView,DetailView
+from django.http import HttpResponse
+
+class HomeTemplateView(TemplateView):
+    template_name = 'index.html'
+    page_nam = 'index'
